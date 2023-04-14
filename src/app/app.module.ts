c9b7d4ffe7d2576047bd6dd1f84ef397c3b24a88
ashim Bera <ashimb07@ashims-Air.Dlink>
2023-04-14 11:07:46 +0530

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UniversityListComponent } from './university-list/university-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UniversityListComponent
   ],
   imports: [
     BrowserModule,
